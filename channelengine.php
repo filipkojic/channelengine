728240f571786339e2cadc616f7ae3bc87bb2de3
Filip Kojic <filip.kojic@logeecom.com>
2024-09-04 13:45:24 +0200

    Add installer class
    ISSUE: MIDU-967

--- channelengine.php
+++ channelengine.php
@@ -24,7 +24,7 @@
  *  International Registered Trademark & Property of PrestaShop SA
  */
 
-use PrestaShopBundle\Entity\Repository\TabRepository;
+use ChannelEngine\PrestaShop\Classes\Utility\ChannelEngineInstaller;
 
 if (!defined('_PS_VERSION_')) {
     exit;
@@ -43,12 +43,6 @@ class ChannelEngine extends Module
 {
     protected $config_form = false;
 
-    /**
-     * ChannelEngine constructor.
-     *
-     * Initializes the module with basic information such as name, tab, version, and author.
-     * It also sets the bootstrap to true to ensure compatibility with PrestaShop's bootstrap framework.
-     */
     public function __construct()
     {
         $this->name = 'channelengine';
@@ -57,7 +51,7 @@ class ChannelEngine extends Module
         $this->author = 'Filip Kojic';
         $this->need_instance = 0;
 
-        $this->bootstrap = true; // Ensure compatibility with bootstrap
+        $this->bootstrap = true;
 
         parent::__construct();
 
@@ -70,119 +64,31 @@ class ChannelEngine extends Module
     /**
      * Install the module.
      *
-     * This method handles the installation of the module, including registering hooks and adding a tab
-     * to the back office.
-     *
      * @return bool True if installation was successful, false otherwise.
      */
     public function install()
     {
-        return parent::install() &&
-            $this->registerHook('header') &&
-            $this->registerHook('displayBackOfficeHeader') &&
-            $this->installTab('AdminParentOrders', 'AdminChannelEngine', 'ChannelEngine')
-            && $this->createCredentialsTable();
-    }
-
-    private function createCredentialsTable()
-    {
-        $sql = 'CREATE TABLE IF NOT EXISTS ' . _DB_PREFIX_ . 'channelengine_credentials (
-                id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                account_name VARCHAR(255) NOT NULL,
-                api_key VARCHAR(255) NOT NULL,
-                date_add DATETIME NOT NULL
-            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;';
-
-        return Db::getInstance()->execute($sql);
-    }
-
-    /**
-     * Install a tab in the back office.
-     *
-     * This method adds a new tab under a specified parent tab in the back office.
-     *
-     * @param string $parent The parent tab's class name.
-     * @param string $class_name The class name of the new tab.
-     * @param string $name The name of the new tab.
-     * @return bool True if the tab was added successfully, false otherwise.
-     */
-    private function installTab($parent, $class_name, $name)
-    {
-        $tab = new Tab();
-        $tabRepository = $this->get('prestashop.core.admin.tab.repository');
-        $tab->id_parent = (int) $tabRepository->findOneIdByClassName($parent);
-        $tab->class_name = $class_name;
-        $tab->module = $this->name;
-        $tab->active = 1;
-
-        foreach (Language::getLanguages(true) as $lang) {
-            $tab->name[$lang['id_lang']] = $name;
-        }
-
-        return $tab->add();
+        $installer = new ChannelEngineInstaller($this);
+        return parent::install() && $installer->install();
     }
 
     /**
      * Uninstall the module.
      *
-     * This method handles the uninstallation of the module, including removing the tab
-     * and any configuration values.
-     *
      * @return bool True if uninstallation was successful, false otherwise.
      */
     public function uninstall()
     {
-        Configuration::deleteByName('CHANNELENGINE_LIVE_MODE');
-        return parent::uninstall() && $this->uninstallTab('AdminChannelEngine')
-             && $this->dropCredentialsTable();
+        $installer = new ChannelEngineInstaller($this);
+        return $installer->uninstall() && parent::uninstall();
     }
 
-    private function dropCredentialsTable()
-    {
-        $sql = 'DROP TABLE IF EXISTS ' . _DB_PREFIX_ . 'channelengine_credentials';
-        return Db::getInstance()->execute($sql);
-    }
-
-    /**
-     * Uninstall a tab from the back office.
-     *
-     * This method removes a tab from the back office.
-     *
-     * @param string $class_name The class name of the tab to remove.
-     * @return bool True if the tab was removed successfully, false otherwise.
-     */
-    private function uninstallTab($class_name)
-    {
-        $tabRepository = $this->get('prestashop.core.admin.tab.repository');
-        $id_tab = (int) $tabRepository->findOneIdByClassName($class_name);
-        if ($id_tab) {
-            $tab = new Tab($id_tab);
-            return $tab->delete();
-        }
-        return false;
-    }
-
-
-    /**
-     * Get content for the configuration page.
-     *
-     * This method redirects the user to the AdminChannelEngine controller for further configuration.
-     *
-     * @return void
-     */
     public function getContent()
     {
         $link = $this->context->link->getAdminLink('AdminChannelEngine');
         Tools::redirectAdmin($link);
     }
 
-    /**
-     * Hook for displaying content in the back office header.
-     *
-     * This method loads specific CSS and JS files when the module's configuration page is accessed.
-     *
-     * @return void
-     */
     public function hookDisplayBackOfficeHeader()
     {
         if (Tools::getValue('configure') == $this->name) {
@@ -191,13 +97,6 @@ class ChannelEngine extends Module
         }
     }
 
-    /**
-     * Hook for displaying content in the front office header.
-     *
-     * This method loads specific CSS and JS files on the front end when needed.
-     *
-     * @return void
-     */
     public function hookHeader()
     {
         $this->context->controller->addJS($this->_path.'/views/js/front.js');
---
+++ classes/Utility/ChannelEngineInstaller.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace ChannelEngine\PrestaShop\Classes\Utility;
+
+use Db;
+use Tab;
+use Language;
+
+/**
+ * Class ChannelEngine
+ *
+ * This class handles the installation,
+ * uninstallation, and configuration of the module, as well as registering necessary hooks
+ * and managing the module's tab in the back office.
+ */
+class ChannelEngineInstaller
+{
+    private $module;
+
+    public function __construct($module)
+    {
+        $this->module = $module;
+    }
+
+    /**
+     * Handles the installation of the module.
+     */
+    public function install()
+    {
+        return $this->createCredentialsTable() &&
+            $this->registerHooks() &&
+            $this->addMenuItem();
+    }
+
+    /**
+     * Handles the uninstallation of the module.
+     */
+    public function uninstall()
+    {
+        return $this->dropCredentialsTable() && $this->removeMenuItem();
+    }
+
+    /**
+     * Creates the credentials table.
+     */
+    private function createCredentialsTable()
+    {
+        $sql = 'CREATE TABLE IF NOT EXISTS ' . _DB_PREFIX_ . 'channelengine_credentials (
+                id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                account_name VARCHAR(255) NOT NULL,
+                api_key VARCHAR(255) NOT NULL,
+                date_add DATETIME NOT NULL
+            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;';
+
+        return Db::getInstance()->execute($sql);
+    }
+
+    /**
+     * Drops the credentials table.
+     */
+    private function dropCredentialsTable()
+    {
+        $sql = 'DROP TABLE IF EXISTS ' . _DB_PREFIX_ . 'channelengine_credentials';
+        return Db::getInstance()->execute($sql);
+    }
+
+    /**
+     * Registers necessary hooks for the module.
+     */
+    private function registerHooks()
+    {
+        return $this->module->registerHook('header') &&
+            $this->module->registerHook('displayBackOfficeHeader');
+    }
+
+    /**
+     * Adds a new tab to the PrestaShop admin menu.
+     */
+    private function addMenuItem()
+    {
+        $tab = new Tab();
+        $tabRepository = $this->module->get('prestashop.core.admin.tab.repository');
+        $tab->id_parent = (int) $tabRepository->findOneIdByClassName('AdminParentOrders');
+        $tab->class_name = 'AdminChannelEngine';
+        $tab->module = $this->module->name;
+        $tab->active = 1;
+
+        foreach (Language::getLanguages(true) as $lang) {
+            $tab->name[$lang['id_lang']] = 'ChannelEngine';
+        }
+
+        return $tab->add();
+    }
+
+    /**
+     * Removes the tab from the PrestaShop admin menu.
+     */
+    private function removeMenuItem()
+    {
+        $tabRepository = $this->module->get('prestashop.core.admin.tab.repository');
+        $id_tab = (int) $tabRepository->findOneIdByClassName('AdminChannelEngine');
+        if ($id_tab) {
+            $tab = new Tab($id_tab);
+            return $tab->delete();
+        }
+        return false;
+    }
+}
